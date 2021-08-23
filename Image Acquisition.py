467fcc3df87cd121976c194980544739d4a56e71
aryansidana <62126688+aryansidana@users.noreply.github.com>
2021-08-23 14:19:29 -0400

    Added saturation check within capture methods.

--- Image Acquisition.py
+++ Image Acquisition.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import picamera
 
 # img --> BGR
-def is_saturated(img):
+def is_Saturated(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     saturation = hsv[:, :, 1];
     saturated_pixels = np.count_nonzero(saturation == 255)
@@ -13,7 +13,8 @@ def is_saturated(img):
     
     return saturated_pixels != 0 and saturated_pixels / (total_pixels - zeros) > 0.1
 
-#Calculate light intensity using HLS image formate
+
+#Calculate light intensity using HLS image format
 def light_HLS(img):
     light= img[:,:,1]
     total_light = np.sum(light)
@@ -35,18 +36,20 @@ def capture():
     
     img = cv2.imread('Desktop/Image_Acquisition/img.png')
 
+    if is_Saturated(img)==True:
+        Print("ERROR: Image is Saturated")
+        return 0;
+    
     #Can adjust threshold cut off value
     th, dst = cv2.threshold(img,0,255,cv2.THRESH_TOZERO)
     
-    imgBRG = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
-    imgHLS = cv2.cvtColor(imgBRG, cv2.COLOR_BGR2HLS)
+    imgHLS = cv2.cvtColor(dst, cv2.COLOR_BGR2HLS)
     
     cam.close()
     return light_HLS(imgHLS)
-
 #Captures and saves FHD image (with angle as name) and returns light intensity
 def capture(angle):
-    cam = picamera.PiCamera
+ `  cam = picamera.PiCamera
     cam.resolution = (1920, 1080)
     cam.exposure_mode('off')
     cam.start_preview()
@@ -56,26 +59,27 @@ def capture(angle):
     
     img = cv2.imread('Desktop/Image_Acquisition/'+angle+'.png')
 
+    if is_Saturated(img)==True:
+        Print("ERROR: Image is Saturated")
+        return 0;
+    
     #Can adjust threshold cut off value
     th, dst = cv2.threshold(img,0,255,cv2.THRESH_TOZERO)
     
-    imgBRG = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
-    imgHLS = cv2.cvtColor(imgBRG, cv2.COLOR_BGR2HLS)
+    imgHLS = cv2.cvtColor(dst, cv2.COLOR_BGR2HLS)
 
     cam.close()
     return light_HLS(imgHLS)
 
 
-def capture(angle,boolean):
+def capture(angle,boolean)
     pass
-
 
 
 
 #Clear matplotlib graph in order to plot new angle range
-def reset():
+def reset()
     pass
-    
 
 
 
